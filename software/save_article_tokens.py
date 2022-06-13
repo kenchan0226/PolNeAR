3e6bab5f48a03725e59f32f869c6f250a783fb90
kenchan0226 <kenchanhp@gmail.com>
2022-06-12 20:27:23 -0500

    install

---
+++ software/save_article_tokens.py
@@ -0,0 +1,32 @@
+from polnear import data
+import pickle as pkl
+
+
+def split_to_paragraphs(split):
+    article_tokens_all = []
+    for article in split:
+        annotated_article = article.annotated()
+        #annotated_article.tokens[0]["attributions"]
+        #annotated_article.tokens[0]["word"]
+        article_tokens = [token for token in annotated_article.tokens]
+        article_tokens_all.append(article_tokens)
+    return article_tokens_all
+
+
+def main(split_name):
+    if split_name == "train":
+        split = data.train()
+    elif split_name == "dev":
+        split = data.dev()
+    elif split_name == "test":
+        split = data.test()
+    else:
+        raise ValueError
+    sample_list = split_to_paragraphs(split)
+    with open(split_name, "wb") as f_out:
+        pkl.dump(sample_list, f_out)
+
+
+if __name__ == "__main__":
+    for split_name in ["train", "dev", "test"]:
+        main(split_name)
