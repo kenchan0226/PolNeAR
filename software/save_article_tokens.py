d2c2ed1d36398eba11299dff44fb0805cc0a055e
kenchan0226 <kenchanhp@gmail.com>
2022-06-13 17:41:00 -0500

    install

--- software/save_article_tokens.py
+++ software/save_article_tokens.py
@@ -10,20 +10,19 @@ def split_to_paragraphs(split):
     article_all = []
     error_count = 0
     for article in tqdm(split):
-        try:
-            annotated_article = article.annotated()
-            #annotated_article.tokens[0]["attributions"]
-            #annotated_article.tokens[0]["word"]
-            #article_tokens = [token for token in annotated_article.tokens]
-            #article_tokens_all.append(article_tokens)
-            attributions_for_sentences = []
-            for sent in annotated_article.sentences:
-                sent_tokens_annotations = [{"word": token["word"], "attributions": list(token["attributions"])} for token in sent.tokens]
-                attributions_for_sentences.append(sent_tokens_annotations)
-            #token_annotations = [{"word": token["word"], "attributions": list(token["attributions"])} for token in annotated_article.tokens]
-            article_all.append({"text": article.text(), "attributions_for_sentences": attributions_for_sentences})
-        except:
-            error_count += 1
+        annotated_article = article.annotated()
+        #annotated_article.tokens[0]["attributions"]
+        #annotated_article.tokens[0]["word"]
+        #article_tokens = [token for token in annotated_article.tokens]
+        #article_tokens_all.append(article_tokens)
+        attributions_for_sentences = []
+        for sent in annotated_article.sentences:
+            sent_tokens_annotations = [{"word": token["word"], "attributions": list(token["attributions"])} for token in sent.tokens]
+            attributions_for_sentences.append(sent_tokens_annotations)
+        #token_annotations = [{"word": token["word"], "attributions": list(token["attributions"])} for token in annotated_article.tokens]
+        article_all.append({"text": article.text(), "attributions_for_sentences": attributions_for_sentences})
+        #except:
+        #    error_count += 1
     print("error count: {}".format(error_count))
     return article_all
 
@@ -37,7 +36,7 @@ def main(split_name):
         split = data.test()
     else:
         raise ValueError
-    sample_list = split_to_paragraphs(split[0:2])
+    sample_list = split_to_paragraphs(split)
     #io.open('filename', 'w', encoding='utf8')
     #split_name + ".json"
     #with io.open(split_name + ".json", 'w', encoding='utf8') as f_out:
